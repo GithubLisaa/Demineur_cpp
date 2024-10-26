5aca675694bd388bf56a18fad33add6ec880323e
AYAPLUSPLUS <lilianlatrille2+githublisaa@gmail.com>
2024-10-26 19:55:52 +0200

    v1

--- Démineur/Démineur.cpp
+++ Démineur/Démineur.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <windows.h>
 
 using namespace std;
 
@@ -7,6 +8,7 @@ struct celldata{
 	bool revealed;
 	bool mine;
 	bool flaged;
+	int minesaround;
 };
 
 celldata gameboard[100][100];
@@ -17,8 +19,96 @@ enum bschoices {
 	GRAND = 24
 };
 
-void iniboard() {
 
+float gennbint(int nb1, int nb2) {
+	return floor(nb1 + ((rand() / (float)RAND_MAX) * nb2));
+}
+
+void color(int color) {
+	switch (color)
+	{
+	case 1:
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+		break;
+	case 2:
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
+		break;
+	case 3:
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE);
+		break;
+	default:
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		break;
+	}
+}
+
+void tempshowmine(int boardsize) {
+	int nbmineonboard = 0;
+	for (int y = 0; y < boardsize; y++)
+	{
+		for (int x = 0; x < boardsize; x++)
+		{
+			if (gameboard[y][x].mine)
+			{
+				cout << "[" << "*" << "]";
+				nbmineonboard++;
+			}
+			else {
+				cout << "[" << " " << "]";
+			}
+		}
+		cout << endl;
+	}
+	cout << nbmineonboard << " mines" << endl;
+}
+
+void iniboard(int boardsize) { 
+	int totalmine = 0;
+
+	switch (boardsize)
+	{
+	case 10:
+		totalmine = 10;
+		break;
+	case 18:
+		totalmine = 40;
+		break;
+	case 24:
+		totalmine = 99;
+		break;
+	default:
+		break;
+	}
+
+	for (int i = 0; i < totalmine; i++)
+	{
+		int x = 0;
+		int y = 0;
+		bool ok = true;
+		bool breakoutfor = false;
+
+		x = gennbint(0, boardsize);
+		y = gennbint(0, boardsize);
+		
+		for (int j = -2; j <= 2 && !breakoutfor; ++j) {
+			for (int k = -2; k <= 2 && !breakoutfor; ++k) {
+				if (y + j >= 0 && y + j < boardsize && x + k >= 0 && x + k < boardsize) {
+					if (gameboard[y + j][x + k].revealed || gameboard[y][x].mine) {
+						ok = false;
+						breakoutfor = true;
+					}
+				}
+			}
+		}
+		if (ok)
+		{
+			gameboard[y][x].mine = true;
+		}
+		else
+		{
+			i--;
+		}
+	}
 }
 
 void afboard(int boardsize) {
@@ -26,22 +116,31 @@ void afboard(int boardsize) {
 	{
 		for (int x = 0; x < boardsize; x++)
 		{
-			if (!gameboard[y][x].revealed && !gameboard[y][x].flaged) {
-				cout << "[" << " " << "]";
+			if (gameboard[y][x].revealed)
+			{
+				if (gameboard[y][x].minesaround > 0) {
+					color(2);
+					cout << "[" << gameboard[y][x].minesaround << "]";
+					color(4);
+				}
+				else {
+					cout << "[" << " " << "]";
+				}
 			}
 			else if (gameboard[y][x].flaged) {
+				color(1);
 				cout << "[" << "F" << "]";
+				color(4);
+			}
+			else {
+				cout << "[" << "?" << "]";
 			}
 		}
 		cout << endl;
 	}
 }
 
-float gennbbt(int nb1, int nb2) {
-	return floor(nb1 + ((rand() / (float)RAND_MAX) * nb2));
-}
-
-void addflag(int boardsize, int x, int y) {
+void editcell(int boardsize, int type, int x, int y) {
 	if (x > boardsize) {
 		x = boardsize - 1;
 	}
@@ -54,7 +153,23 @@ void addflag(int boardsize, int x, int y) {
 	else if (y < 0) {
 		y = 0;
 	}
-	gameboard[y][x].flaged = true;
+	switch (type)
+	{
+	case 1:
+		if (!gameboard[y][x].flaged)
+		{
+			gameboard[y][x].flaged = true;
+		}
+		else
+		{
+			gameboard[y][x].flaged = false;
+		}
+		break;
+	case 2:
+		gameboard[y][x].revealed = true;
+	default:
+		break;
+	}
 }
 
 int main()
@@ -66,8 +181,13 @@ int main()
 	int select = 0;
 	int x = 0;
 	int y = 0;
+	bool firstsele = true;
 
-	cout << "Entrer une taille de tableau\n1. PETIT\n2. MOYEN\n3. GRAND\n> ";
+	gennbint(0, 0);
+	gennbint(0, 0);
+	gennbint(0, 0);
+
+	cout << "Entrer une taille de tableau:\n1. PETIT\n2. MOYEN\n3. GRAND\n> ";
 	cin >> taille;
 
 	switch (taille)
@@ -88,7 +208,7 @@ int main()
 
 	while (true)
 	{
-		cout << endl << "Demineur\n1. Afficher le tableau\n2. Poser un drapeau\n> ";
+		cout << endl << "Demineur\n1. Afficher le tableau\n2. Decouvrire une case\n3. Poser un drapeau\n>  ";
 		cin >> select;
 
 		switch (select)
@@ -97,13 +217,30 @@ int main()
 			afboard(boardsize);
 			break;
 		case 2:
-			cout << "Entrer une coordonnee x et y\nEntrer x\n> ";
+			cout << "Entrer une coordonnee x et y\nEntrer x:\n> ";
 			cin >> x;
-			cout << "Entrer y\n> ";
+			cout << "Entrer y:\n> ";
 			cin >> y;
-			addflag(boardsize, x - 1, y - 1);
+
+			if (firstsele) {
+				editcell(boardsize, 2, x - 1, y - 1);
+				iniboard(boardsize);
+				firstsele = false;
+			}
+			else
+			{
+				editcell(boardsize, 2, x - 1, y - 1);
+			}
 			break;
 		case 3:
+			cout << "Entrer une coordonnee x et y\nEntrer x:\n> ";
+			cin >> x;
+			cout << "Entrer y:\n> ";
+			cin >> y;
+			editcell(boardsize, 1, x - 1, y - 1);
+			break;
+		case 4:
+			tempshowmine(boardsize);
 			break;
 		default:
 			cout << "Erreur, saissie incorrect";
